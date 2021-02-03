51da499331c9eaa61a99212fe314b7c83e94e591
petros-petrosyan-51 <multimontana@gmail.com>
2021-02-03 16:43:57 +0400

    Innitial Commit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
 function appendResult(data){
+let url = ""    
+if (!data[0].StartTime){
+  url =  "https://www.novibet.com/live-betting?event="+data[0].BetContextId
+}else{
+  url = "https://www.novibet.com/sports/"+data[0].Path+"/e"+data[0].BetContextId;  
+}
+function copyToClipboard(text) {
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val(text).select();
+    document.execCommand("copy");
+    $temp.remove();
+  }
 data.map(({Path,BetContextId,AdditionalCaptions,StartTime,name})=>{
      name = AdditionalCaptions.Competitor1
      if (!StartTime){
@@ -10,11 +23,10 @@ data.map(({Path,BetContextId,AdditionalCaptions,StartTime,name})=>{
      name = name + " - "+AdditionalCaptions.Competitor2
      $(".result").append('<div class="main"><div><p>'+name+'</p><p class="time">'+StartTime+'</p></div><a href="'+Path+'">Link</a></div>')
  })
+ copyToClipboard(url)
 }
 function events(){
     if ($(".search").val()){
-        $(".result").empty();
-        $(".result").append(' <div class="main">Loading...</div>')
         $.ajax({
             url: window.location.pathname+$(".search").val(),
             method: "POST",
@@ -27,22 +39,38 @@ function events(){
                 $(".result").empty();
                 $(".result").append(' <div class="main">No Result</div>')
              }  
-             appendResult(data.LiveNow);
-             appendResult(data.LiveSchedule);
+             if (data.LiveNow.length !== 0){
+             appendResult(data.LiveNow)
+             }
+             if (data.LiveSchedule.length !== 0){
+             appendResult(data.LiveSchedule)
+             }
+            },
+            error: function(){
+              console.clear();  
+              $(".result").empty();
+              $(".result").append(' <div class="main">Not Found</div>')
             }
         })
        }
 }
-$( document ).ready(function() {
-   $("#novibet").click(function(){
-     $(".novibet").toggle(500)
-   })   
-   $(".search").on('keypress',function(e) {
-    if(e.which == 13) {
-        events();
-    }
-});
-   $('#search').click(function(){
-       events();
-   })
+$(document).on('paste', function(e){
+    $(".search").val('');
+    $(".result").empty();
+    $(".result").append(' <div class="main">Loading...</div>');
+    $(".search").focus();
+    setTimeout(function(){
+       events()
+         }, 500);
+ })
+$( document ).ready(function(){
+    $(".search").focus();
+     $(document).click(function(e){
+        $(".search").focus();
+    });
+    $(".search").on('keypress',function(e) {
+        if(e.which == 13) {
+            events();
+        }
+    });
 });
